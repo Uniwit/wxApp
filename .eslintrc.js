b56aadbe867b2b556bd23fc3369cd847105746e5
mark1.zhang <mark1.zhang@liulishuo.com>
2018-06-08 18:18:17 +0800

    header

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,12 +18,15 @@ module.exports = {
   },
   // add your custom rules here
   'rules': {
+    "semi": [2, "always"],
+    "comma-dangle": [1, "always"],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    'no-console': 0,
   }
 }
